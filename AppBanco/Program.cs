2042ab3cbff8964edc568beb5e14ecf16e97f0bf
3ds <3ds@LAB2-14>
2023-08-04 10:58:05 -0300

    Adicionar arquivos de projeto.

---
+++ AppBanco/Program.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection Conexao = new MySqlConnection(@"Server = Localhost; database = dbAppBanco; user = root; password = 12345678");
+            
+            Conexao.Open();
+
+            MySqlCommand ObjCommand = new MySqlCommand();
+            ObjCommand.CommandText = "select * from tbUsuario";
+            ObjCommand.CommandType = System.Data.CommandType.Text;
+            ObjCommand.Connection = Conexao;
+
+            Console.WriteLine("Banco Conectado!");
+            Console.ReadLine();
+        }
+    }
+}
